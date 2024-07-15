bb464e2c3e3d2cda998c6004e32c155997b7e368
Yasadi12 <yasadinethmini@gmail.com>
2024-07-15 14:00:27 +0530

    install react js and tailwid css and create the first template

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import SingUp from './pages/SingUp';
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Header />
+
+    <Routes>
+     <Route path="/" element={<Home />} />
+     <Route path="/sign-in" element={<Signin />} />
+     <Route path="/sign-up" element={<SingUp />} />
+     <Route path="/about" element={<About />} />
+     <Route path="/profile" element={<Profile />} />
+
+    </Routes>
+     </BrowserRouter>
   )
 }
 
